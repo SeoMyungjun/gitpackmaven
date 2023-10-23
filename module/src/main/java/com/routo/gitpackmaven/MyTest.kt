4da4488177f17e704036ab4e05f94de30f17e5d1
지도서비스플랫폼팀/서명준 <smj@hyundai-autoever.com>
2023-10-23 16:15:57 +0900

    Initial commit

---
+++ module/src/main/java/com/routo/gitpackmaven/MyTest.kt
@@ -0,0 +1,13 @@
+package com.routo.gitpackmaven
+
+import android.content.Context
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import javax.naming.Context
+
+class MyTest(context: Context) {
+  fun test(context: Context, message:String) {
+      Toast.makeText()
+  }
+}
